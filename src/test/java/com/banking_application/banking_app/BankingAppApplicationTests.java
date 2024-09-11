2b1874ab5cd94bf0a1d7ec946b1d103df0cdd406
Rishon <rishon.kumar.rahi@gmail.com>
2024-09-11 09:03:17 +0530

    Setting up the project

---
+++ src/test/java/com/banking_application/banking_app/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking_application.banking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
